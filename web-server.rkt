1d000b0716bdeef9880384a951dbc5674b59d02e
bruno ladeia <brunoladeia13@gmail.com>
2018-08-03 00:29:10 +0000

    Adicionado funções que recebem os dados tratados do BD e ajustam eles para serem inseridos numa tabela (in progress).

--- web-server.rkt
+++ web-server.rkt
@@ -4,15 +4,27 @@
 ;struct padrao de uma linha da tabela
 (struct table (data hora esporte local jogadores))
 
+;retorna uma lista de lista com todos os elementos do BD
+(define (gera-lista-linhas)
+  (vectorList->list))
+
+;converte uma lista na estrutura de tabela
+(define (list->table lst)
+      (table (car lst) (car (cdr lst)) (car (cddr lst)) (car (cdddr lst)) (car (cddddr lst))))
+
+;converte a lista vinda do BD, numa lista de tabelas
+(define (listOfTables)
+  (define (listOfTables2 lst tb)
+    (if (null? lst)
+        tb
+        (listOfTables2 (cdr lst) (cons (list->table (car lst)) tb))))
+  (listOfTables2 (gera-lista-linhas) null))
+
 ;define as linhas da tabela em html
 (define TABELA
-  (table "data" "hora" "esporte" "local" "jogadores"))
+  (cons (table "data" "hora" "esporte" "local" "jogadores") (listOfTables)))
 
-;retorna uma lista de vetores, ta dificil mexer com isso, nao acho mta coisa na net
-(define (gera-lista-linhas)
-  (length(lista-tudo)))
-
-;gera as colunas cabecalho da tabela
+;gera as colunas cabecalhos da tabela
 (define (th-html tbl)
  `(th,(table-data tbl)
   (th,(table-hora tbl))
@@ -23,7 +35,11 @@
 ;gera o html da tabela
 (define (gera-tabela-html tbl)
   `(div ((class "table"))
-        (table(tr(,@(th-html tbl))))))
+        (table(,@(vai-linha tbl)))))
+
+;brincando com fogo
+(define (vai-linha tbl)
+  (and (null? tbl) `(tr(,@(th-html (car tbl)))) (vai-linha (cdr tbl)))))
         
 ;start no web-server       
 (define (start request)
@@ -35,6 +51,3 @@
    `(html (head (title "Mural da quadra"))
           (body (h1 "Mural da quadra")
                 ,(gera-tabela-html tbl)))))
-
-
-  
