8b6f92c3758bcfd222ead51f2720e3fc51a71ecb
Gerson Ferreira Junior <gerson.fj@gmail.com>
2018-08-05 17:08:36 -0300

    Adicionado função que gera uma String com o codigo html, recebendo como parâmetro uma lista de listas, onde cada lista representa uma tupla do banco de dados

--- front-end.rkt
+++ front-end.rkt
@@ -2,7 +2,10 @@
 
 (require scribble/html)
 
-;Gera  o código html dos titulos de cada coluna da tabela
+
+;*******************Funções auxiliares*******************
+
+;(String) Gera  o código html dos titulos de cada coluna da tabela
 (define (gera-titulos)
   (xml->string
    (thead
@@ -13,33 +16,40 @@
      (th "local")
      (th "jogadores")))))
 
-;Função auxiliar que gera uma tag </th> com o primeiro elemento de uma lista 
+;(String) Função auxiliar que gera uma tag </th> com o primeiro elemento de uma lista 
 (define (gera-th lst)
     (xml->string
        (th (car lst)) ))
 
-;Gera o código HTML de uma linha com dados recebidos de uma lista, no caso, esperando: (Esporte Horario)
+;(String) Gera o código HTML de apenas uma linha com dados recebidos de uma lista, abrindo e fechando com a tag <tr>
 (define (gera-linha lst)
   (define (gera-linha-aux l acc)
     (if (not (null? l))
         (gera-linha-aux (cdr l) (string-append acc (gera-th l)))
-        acc))
+        (xml->string
+         (tr (literal acc)))))
   (gera-linha-aux lst ""))
 
-;Função auxiliar que gera uma tag </tr> com uma tupla de dados
-(define (gera-tr lst)
-  (xml->string
-   (tr (gera-linha lst))))
-
-;Gera o código html de uma tabela recebendo uma lista de listas como parâmetro. Cada lista da lista representará uma tupla da tabela
+;(String) Gera o código html de uma tabela recebendo uma lista de listas como parâmetro. Cada lista da lista representará uma tupla da tabela
 (define (gera-tabela lst)
   (define (gera-tabela-aux l acc)
     (if (not (null? l))
-        (gera-tabela-aux (cdr l) (string-append acc (gera-tr (car l))))
+        (gera-tabela-aux (cdr l) (string-append acc (gera-linha (car l))))
         acc))
-  (gera-tabela-aux lst (gera-titulos)))
+  (gera-tabela-aux lst ""))
 
-
-
-
-  
+;(String) Gera a página htlm com dados da tabela
+(define (gera-pagina lst)
+  (xml->string
+   (html
+    (head
+     (make-element 'meta '[(charset . "utf-8")
+                           (name . "viewport")
+                           (content . "initial-scale=1.0; maximum-scale=1.0; width=device-width;")] "")
+     (title "Mural da Quadra"))
+    (body
+     (make-element 'div '[(class . "table-title")]
+      (h1 "Mural da Quadra"))
+     (table
+      (make-element 'di '[(class . "table-title")] (literal gera-titulos))
+      (make-element 'tbody '[(class . "table-hover")] (literal (gera-tabela lst))))))))
